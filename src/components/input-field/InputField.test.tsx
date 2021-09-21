d012c0f9c7fa3916e0640dc2b2714c3fa885f1a8
Imran Sheikh <imransheikhsadi@gmail.com>
2021-09-21 08:41:05 +0600

    Delete src directory

--- src/components/input-field/InputField.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import InputField from './InputField.component';
-
-test('renders learn react link', () => {
-    render(<InputField />);
-    //   const linkElement = screen.getByText(/learn react/i);
-    //   expect(linkElement).toBeInTheDocument();
-});
--- src/components/user-view/UserView.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import UserView from './UserView.component';
-
-test('renders learn react link', () => {
-    render(<UserView />);
-    //   const linkElement = screen.getByText(/learn react/i);
-    //   expect(linkElement).toBeInTheDocument();
-});
